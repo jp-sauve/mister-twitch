96e6b81d133bd1721b5952d252fac301e008c8d6
jp-sauve <jean-paul@grokology.ca>
2017-10-02 19:18:37 -0400

    adds both online and offline info to page. fixes #4, fixes #3.

--- src/composeTwitchStreamersList.js
+++ src/composeTwitchStreamersList.js
@@ -9,65 +9,60 @@ let debug = true,
 
 function getTList(...allProps) {
   let [streamers, ...moreProps] = allProps,
-    streamerRequests = [],
+  streamerRequests = [],
     channelRequests = [],
     baseURLonline = 'https://wind-bow.glitch.me/twitch-api/streams/',
-    baseURLoffline = 'https://wind-bow.glitch.me/twitch-api/channels/',
-    composedListHTML = '';
-if (allProps) {console.log("You've given unexpected properties to getTList function!\n");}
+    baseURLoffline = 'https://wind-bow.glitch.me/twitch-api/channels/';
+
   for (let [index, streamer] of streamers.entries()) {
     streamerRequests.push($.getJSON(baseURLonline + streamer, function(results) {
       return results;
     }));
-
     channelRequests.push($.getJSON(baseURLoffline + streamer, function(results) {
       return results;
     }));
-
   }
   let onlinePromises = Promise.all(streamerRequests),
     offlinePromises = Promise.all(channelRequests);
 
   Promise.all([onlinePromises, offlinePromises])
-    .then(results => {
+    .then(allResults => {
 
-      results[0].forEach(function(item) {
-        console.log("\nCOMPLETED PROMISES 0\n" + JSON.stringify(item));
-      });
-      results[1].forEach(function(item) {
-        console.log("\nCOMPLETED PROMISES 2\n" + JSON.stringify(item));
-      });
+      for (let [index, streamerResult] of allResults[0].entries()) {
+        if (streamerResult.stream !== null) {
+          const streamerStrippedResults = {
+            stream: {
+            preview: {
+              large: streamerResult.stream.preview.large,
+            },
+            channel: {
+              display_name: streamerResult.stream.channel.display_name,
+              status: streamerResult.stream.channel.status,
+              game: streamerResult.stream.channel.game,
+            }
+          }
+          };
+          console.log("\nOnline:\n" + JSON.stringify(streamerStrippedResults,null,'\t'));
+          addHTMLToList(onlinetmpl.render(streamerStrippedResults));
+        } else {
+          const channelStrippedResults = {
+            channel: {
+              display_name: allResults[1][index].display_name,
+              url: allResults[1][index].url,
+              logo: allResults[1][index].logo || "/mister-twitch/images/logo-transp-dark-300.png"
+            }
+          };
+          console.log("\nOffline:\n" + JSON.stringify(channelStrippedResults,null,'\t'));
+          addHTMLToList(offlinetmpl.render(channelStrippedResults));
+        }
+      }
+    });
 
-    })
 };
 
-function stripResults(...rawResults) {
-  let onlineListedFirst = (rawResults[0].stream !== null);
-
-  const onlineStrippedResults = {
-    stream: results.stream,
-    preview: {
-      large: results.stream.preview.large,
-    },
-    channel: {
-      display_name: results.stream.channel.display_name,
-      status: results.stream.channel.status,
-      game: results.stream.channel.game,
-    }
-  };
-  const offlineStrippedResults = {
-    channel: {
-      display_name: results.display_name,
-      url: results.url,
-      logo: results.logo || "http://default.image"
-    }
-  };
-}
-
-
 function addHTMLToList(renderedHTML) {
-  listHTML += renderedHTML;
-  $('#streamList').html();
+  let composedListHTML = renderedHTML;
+  $('#streamList').append(composedListHTML);
 }
 export {
   getTList
